093c031081284c201758c423ad80b025bf7eb4bf
Matt Stratton <matt.stratton@gmail.com>
2015-04-26 23:22:36 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# training_mysql
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'training_mysql'
+maintainer       'Matt Stratton'
+maintainer_email 'mattstratton@chef.io'
+license          'apache2'
+description      'Installs/Configures training_mysql'
+long_description 'Installs/Configures training_mysql'
+version          '0.1.0'
+
